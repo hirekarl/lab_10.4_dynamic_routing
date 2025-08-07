695d4e047da5e0946b6e3c4ba4cdebab0c26eb3a
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-07 15:02:01 -0400

    Implemented naive navbar.

--- dynamic-routing/src/App.tsx
+++ dynamic-routing/src/App.tsx
@@ -1,7 +1,69 @@
 import "./App.css"
+import { Routes, Route, Link, NavLink } from "react-router-dom"
+import AdminPage from "./components/AdminPage"
+import BlogIndex from "./components/BlogIndex"
+import BlogDetail from "./components/BlogDetail"
+import LoginPage from "./components/LoginPage"
 
 const App = () => {
-  return <></>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand">
+            My Life with Honk: A Canadian Mail Tale
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="#navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  to="/blog"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }>
+                  Blog
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }>
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/admin"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }>
+                  Admin
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/blog" element={<BlogIndex />} />
+        <Route path="/blog/:slug" element={<BlogDetail />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
