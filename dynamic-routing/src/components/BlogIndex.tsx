9f0938a8f070feff6c5eb9c46e499f9f1f71e9a9
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-07 18:18:05 -0400

    Added <time> element to BlogIndex items.

--- dynamic-routing/src/components/BlogIndex.tsx
+++ dynamic-routing/src/components/BlogIndex.tsx
@@ -8,10 +8,17 @@ const BlogIndex = ({ posts }: BlogIndexProps) => {
     <li key={p.id} className="list-group-item">
       <div className="d-flex justify-content-between align-items-center">
         <div>
-          <Link className="link-underline link-underline-opacity-0" to={p.slug}>{p.title}</Link>
+          <Link className="link-underline link-underline-opacity-0" to={p.slug}>
+            {p.title}
+          </Link>
         </div>
         <div>
-          <em>Published {timestampToDisplayDate(p.timestamp)}</em>
+          <em>
+            Published&nbsp;
+            <time dateTime={new Date(p.timestamp).toISOString()}>
+              {timestampToDisplayDate(p.timestamp)}
+            </time>
+          </em>
         </div>
       </div>
     </li>
